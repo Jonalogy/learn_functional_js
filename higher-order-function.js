0053a9e432e3aef93177b6fe4d24c54a07279d3e
Jonathan Tan <Jonalogy@Jonathans-MacBook-Air.local>
2017-05-29 21:40:36 +0800

    Converted to ES6 syntax

--- higher-order-function.js
+++ higher-order-function.js
@@ -1,14 +1,17 @@
 let square = x => x * x
 
-let pi = () => 3.14159
+let piApprox = () => 22/7
 
 let multiply = ( a , b )=>( a * b )
 
 let areaCircle = p =>(
-  radius => multiply( p() , square(radius) )
+  (radius) => {
+    let area = multiply(p() , square(radius))
+    return `Area of a circle of radius ${radius} is approximately ${area.toFixed(3)} unit square`
+  }
 )
 
-let circle = areaCircle(pi)
+let circle = areaCircle(piApprox)
 let circle_radius_1 = circle(1)
 
 console.log(circle_radius_1);
